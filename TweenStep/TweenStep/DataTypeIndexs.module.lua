5194266865bb0e26cfd7a5462a4362aefd8ab130
Davvex87 <102032123+Davvex87@users.noreply.github.com>
2023-04-06 12:31:31 +0000

    Create DataTypeIndexs.module.lua

---
+++ TweenStep/TweenStep/DataTypeIndexs.module.lua
@@ -0,0 +1,28 @@
+return {
+	Axes = Axes.new;
+	--BrickColor = BrickColor.new;		BRICK COLOR CANNOT TWEEN VALUES, WE HAVE TO USE .Color, BUT I'M TOO LAZY TO IMPLEMENT SO :)
+	CFrame = CFrame.new;
+	Color3 = Color3.new;
+	Vector2 = Vector2.new;
+	Vector3 = Vector3.new;
+	Vector2int16 = Vector2int16.new;
+	Vector3int16 = Vector3int16.new;
+	UDim = UDim.new;
+	UDim2 = UDim2.new;
+	Rect = Rect.new;
+	Region3 = Region3.new;
+	Region3int16 = Region3int16.new;
+	
+	["#Axes"] = -1;
+	["#CFrame"] = 12;
+	["#Color3"] = 3;
+	["#Vector2"] = 2;
+	["#Vector3"] = 3;
+	["#Vector2int16"] = 2;
+	["#Vector3int16"] = 3;
+	["#UDim"] = 2;
+	["#UDim2"] = 4;
+	["#Rect"] = 4;
+	["#Region3"] = 2;
+	["#Region3int16"] = 2;
+}
